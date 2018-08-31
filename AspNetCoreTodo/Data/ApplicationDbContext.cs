ee9c2422c97362e9173b469895893b54a6b5b89d
Pablo Scuteri <pabloscuteri@gmail.com>
2018-08-31 14:03:14 -0300

    Se inicio el agrgado de la persistencia en la base de datos. Se creo el DbSet que contiene la informacion de los Objetos TodoItem

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -22,5 +22,8 @@ namespace AspNetCoreTodo.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet< TodoItem> Items { get; set; }
+
     }
 }
